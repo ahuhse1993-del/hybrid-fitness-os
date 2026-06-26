ce001d4db6c6f3a6c8155fb76184bd5b8ca46cd7
Alexander <a.huhse.1993@gmail.com>
2026-06-26 14:55:23 +0200

    Add splits import to activities sync

--- data/garmin_sync_all.py
+++ data/garmin_sync_all.py
@@ -1,7 +1,7 @@
 """
 CAIRN Garmin Full Sync
 health: Schlaf, HRV, Body Battery (morgens)
-activities: Neue Trainings (alle 2h)
+activities: Neue Trainings + Splits (alle 2h)
 """
 
 import sys, os
@@ -24,6 +24,10 @@ else:
     print("--- Aktivitäten Sync ---")
     from data.garmin_import_history import import_all_activities
     import_all_activities()
+
+    print("\n--- Splits Sync ---")
+    from data.garmin_import_splits import import_splits
+    import_splits()
 
     print("\n--- Kalender Sync ---")
     from data.garmin_calendar_sync import sync_garmin_calendar
